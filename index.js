2777007533a18645f186187b0edf1cbc3fb94c5c
Andrii <whiteyelnikov@gmail.com>
2024-10-31 08:45:46 +0200

    first commit

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true} ));
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(router);
 app.use(errorMiddleware);
 
